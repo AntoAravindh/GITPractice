2387191ebb26cb96a21b0742d9808db8ff0fbf8e
AntoAravindh <anto.aravindh100@gmail.com>
2024-06-22 16:05:26 +0530

    first commit

--- src/org/samepackage/javaDemo.java
+++ src/org/samepackage/javaDemo.java
@@ -2,16 +2,7 @@ package org.samepackage;
 
 public class javaDemo {
 	public static void main(String[] args) {
-		int a=10;
-		int b =20;
-		
-		a=a+b;
-	    b=a-b;
-		a=a-b;
-		
-		
-		System.out.println(a);
-		System.out.println(b);
+		//dinesh
 	}
 
 }
